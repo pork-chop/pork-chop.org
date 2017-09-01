4ef7dcf33793f49041596f0a33c08e22a80cbe78
Amy Dalrymple <adalrymple@jtmega.com>
2017-09-01 13:34:09 -0500

    floating share bar plugin update

---
+++ wp-content/plugins/sharefloat_plugin/includes/class-sharefloat-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://amydalrymple.org/
+ * @since      1.0.0
+ *
+ * @package    sharefloat
+ * @subpackage sharefloat/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    sharefloat
+ * @subpackage sharefloat/includes
+ * @author     Amy Dalrymple <amy@tenaciousedge.com>
+ */
+class sharefloat_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
